6ebdd3cd8f1aaafd139df2952ff55ce2d49fd848
DEV <DEV@gmail.com>
2020-04-18 23:43:21 +0530

    PdfView Completed

--- SpringMVCProj-BeanNameViewResolver/src/main/java/com/nt/view/EmployeePdfView.java
+++ SpringMVCProj-BeanNameViewResolver/src/main/java/com/nt/view/EmployeePdfView.java
@@ -1,5 +1,7 @@
 package com.nt.view;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,14 +10,37 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
+import com.nt.dto.EmployeeDTO;
 
 public class EmployeePdfView extends AbstractPdfView {
 
 	@Override
-	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+	protected void buildPdfDocument(Map<String, Object> map, Document doc, PdfWriter writer,
+			HttpServletRequest req, HttpServletResponse res) throws Exception {
+        res.addHeader("Content-Disposition", "attachment;filename=emp.pdf");
+		Paragraph p=new Paragraph("Employee Report");
+		doc.add(p);
+		List<EmployeeDTO> listDTO=(List<EmployeeDTO>) map.get("listDTO");
+		//craete table and add to Document
+		PdfPTable table=new PdfPTable(5);
+		table.addCell("SR. NUM");
+		table.addCell("EMP NUM");
+		table.addCell("NAME");
+		table.addCell("JOB");
+		table.addCell("SALARY");
+		for(EmployeeDTO dto:listDTO) {
+			table.addCell(String.valueOf(dto.getSrNo()));
+			table.addCell(String.valueOf(dto.getEmpNo()));
+			table.addCell(dto.getEmpName());
+			table.addCell(dto.getJob());
+			table.addCell(String.valueOf(dto.getSalary()));    
+		}
+
+		doc.add(table);
+		doc.add(new Paragraph(new Date().toString()));
 
 	}
 
